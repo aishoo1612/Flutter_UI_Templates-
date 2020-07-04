bc0714dba0280b7aa3c31a5751fa74e0b3a2d5f2
Shatakshi Raman <43930277+aishoo1612@users.noreply.github.com>
2020-07-04 16:35:14 +0530

    For the UI, go to the following behance :
    https://xd.adobe.com/view/697d31bf-fc29-4b20-8668-fa05ddbcea34-3c2e/

---
+++ iPhoneXXS11Pro2.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import './iPhoneXXS11Pro3.dart';
+
+class iPhoneXXS11Pro2 extends StatelessWidget {
+  iPhoneXXS11Pro2({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff0d0101),
+      body: Stack(
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset(53.0, 452.0),
+            child: Container(
+              width: 270.0,
+              height: 53.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(38.0),
+                color: const Color(0x635ca9af),
+                border: Border.all(width: 1.0, color: const Color(0x635ca9af)),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(307.0, -473.0),
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(-532.74, 393.0),
+                  child: SvgPicture.string(
+                    _svg_2qzd95,
+                    allowDrawingOutsideViewBox: true,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(182.0, 242.0),
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(-269.0, 393.0),
+                  child: Container(
+                    width: 216.0,
+                    height: 260.0,
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.all(Radius.elliptical(108.0, 130.0)),
+                      color: const Color(0xfff37a83),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(-91.0, 509.0),
+                  child: Container(
+                    width: 151.0,
+                    height: 144.0,
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.all(Radius.elliptical(75.5, 72.0)),
+                      color: const Color(0xff5ca9af),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(-408.0, 480.0),
+                  child: Container(
+                    width: 162.0,
+                    height: 173.0,
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.all(Radius.elliptical(81.0, 86.5)),
+                      color: const Color(0xff0e2c4e),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(4.0, 452.0),
+            child: SizedBox(
+              width: 367.0,
+              height: 77.0,
+              child: Text(
+                'LOGIN',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 45,
+                  color: const Color(0xfff37a83),
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(8.0, 138.0),
+            child:
+                // Adobe XD layer: 'image-removebg-prev…' (shape)
+                Container(
+              width: 360.0,
+              height: 240.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: const AssetImage(''),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(53.0, 536.0),
+            child: Container(
+              width: 270.0,
+              height: 53.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(38.0),
+                color: const Color(0x87f37a83),
+                border: Border.all(width: 1.0, color: const Color(0x87f37a83)),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(8.0, 536.0),
+            child: SizedBox(
+              width: 367.0,
+              height: 77.0,
+              child: Text(
+                'SIGN UP',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 45,
+                  color: const Color(0xff5ca9af),
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const String _svg_2qzd95 =
+    '<svg viewBox="-532.7 393.0 592.7 260.0" ><path transform="translate(-269.0, 393.0)" d="M 108 0 C 167.6467437744141 0 216 58.20298767089844 216 130 C 216 201.7970123291016 138.6454620361328 130 78.99871826171875 130 C 19.35197067260742 130 0 201.7970123291016 0 130 C 0 58.20298767089844 48.35325241088867 0 108 0 Z" fill="#f37a83" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(-91.0, 509.0)" d="M 75.5 100.1680526733398 C 117.1974945068359 100.1680526733398 151 32.23550033569336 151 72 C 151 111.7644958496094 117.1974945068359 144 75.5 144 C 33.8025016784668 144 0 111.7644958496094 0 72 C 0 32.23550033569336 33.80250549316406 100.1680526733398 75.5 100.1680526733398 Z" fill="#5ca9af" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(-477.0, 480.0)" d="M 115.5 0 C 179.2888793945313 0 24.83791351318359 -27.12204742431641 24.83791351318359 20.65067291259766 C 24.83791351318359 68.42326354980469 179.2888793945313 173 115.5 173 C 51.71111679077148 173 -55.74429321289063 143.6334075927734 -55.74429321289063 95.86073303222656 C -55.74429321289063 61.45991134643555 -33.39210891723633 20.18029975891113 16.24248886108398 42.13926315307617 C 32.82754135131836 49.4767951965332 130.8730163574219 127.6918640136719 146.2484283447266 127.0191879272461 C 148.3451538085938 126.927360534668 113.2030334472656 0 115.5 0 Z" fill="#0e2c4e" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
---
+++ iPhoneXXS11Pro3.dart
@@ -0,0 +1,457 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import './iPhoneXXS11Pro4.dart';
+
+class iPhoneXXS11Pro3 extends StatelessWidget {
+  iPhoneXXS11Pro3({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff0d0101),
+      body: Stack(
+        children: <Widget>[
+          Transform.translate(
+            offset: Offset(160.0, 314.0),
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(-269.0, 403.0),
+                  child: Container(
+                    width: 216.0,
+                    height: 260.0,
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.all(Radius.elliptical(108.0, 130.0)),
+                      color: const Color(0xfff37a83),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(-99.0, 468.0),
+                  child: Container(
+                    width: 151.0,
+                    height: 144.0,
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.all(Radius.elliptical(75.5, 72.0)),
+                      color: const Color(0xff5ca9af),
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(-408.0, 480.0),
+                  child: Container(
+                    width: 162.0,
+                    height: 173.0,
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.all(Radius.elliptical(81.0, 86.5)),
+                      color: const Color(0xff0e2c4e),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(26.0, 116.0),
+            child: Container(
+              width: 90.0,
+              height: 90.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(71.0),
+                image: DecorationImage(
+                  image: const AssetImage(''),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(90.0, -61.0),
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(-9.81, 204.83),
+                  child: SizedBox(
+                    width: 145.0,
+                    height: 111.0,
+                    child: Text.rich(
+                      TextSpan(
+                        style: TextStyle(
+                          fontFamily: 'Lato',
+                          fontSize: 20,
+                          color: const Color(0xfff37a83),
+                        ),
+                        children: [
+                          TextSpan(
+                            text: '13\n',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Hours\n',
+                            style: TextStyle(
+                              color: const Color(0xff5ca9af),
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(222.0, 245.0),
+                  child: SizedBox(
+                    width: 52.0,
+                    height: 1.0,
+                    child: SingleChildScrollView(
+                        child: Text(
+                      '\n',
+                      style: TextStyle(
+                        fontFamily: 'Lato',
+                        fontSize: 20,
+                        color: const Color(0xff5ca9af),
+                        fontWeight: FontWeight.w700,
+                      ),
+                      textAlign: TextAlign.center,
+                    )),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(77.19, 204.83),
+                  child: SizedBox(
+                    width: 145.0,
+                    height: 111.0,
+                    child: Text.rich(
+                      TextSpan(
+                        style: TextStyle(
+                          fontFamily: 'Lato',
+                          fontSize: 20,
+                          color: const Color(0xfff37a83),
+                        ),
+                        children: [
+                          TextSpan(
+                            text: '1983\n',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'XP\n',
+                            style: TextStyle(
+                              color: const Color(0xff5ca9af),
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                Transform.translate(
+                  offset: Offset(169.19, 205.83),
+                  child: SizedBox(
+                    width: 145.0,
+                    height: 111.0,
+                    child: SingleChildScrollView(
+                        child: Text.rich(
+                      TextSpan(
+                        style: TextStyle(
+                          fontFamily: 'Lato',
+                          fontSize: 20,
+                          color: const Color(0xfff37a83),
+                        ),
+                        children: [
+                          TextSpan(
+                            text: '13\n',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Poses\n\n\n',
+                            style: TextStyle(
+                              color: const Color(0xff5ca9af),
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.center,
+                    )),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(282.26, -448.56),
+            child: Stack(
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(-258.98, 372.19),
+                  child: SvgPicture.string(
+                    _svg_xnw4oi,
+                    allowDrawingOutsideViewBox: true,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(0.0, 213.0),
+            child: SizedBox(
+              width: 146.0,
+              height: 55.0,
+              child: Text.rich(
+                TextSpan(
+                  style: TextStyle(
+                    fontFamily: 'Lato',
+                    fontSize: 20,
+                    color: const Color(0xff5ca9af),
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Emily Warren',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '\n@99emilyW',
+                      style: TextStyle(
+                        color: const Color(0xfff37a83),
+                      ),
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(41.0, 291.0),
+            child: SizedBox(
+              width: 294.0,
+              child: Text(
+                'Yoga MOM, on a Coffee run\nScroll for fun',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 20,
+                  color: const Color(0xfffafafa),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(188.0, 212.0),
+            child:
+                // Adobe XD layer: 'image-removebg-prev…' (shape)
+                Container(
+              width: 66.0,
+              height: 44.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(27.0),
+                image: DecorationImage(
+                  image: const AssetImage(''),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(235.0, 201.0),
+            child:
+                // Adobe XD layer: 'image-removebg-prev…' (shape)
+                Container(
+              width: 66.0,
+              height: 66.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: const AssetImage(''),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(-7.0, 362.0),
+            child: SizedBox(
+              width: 146.0,
+              child: Text(
+                'Photos  (96) >\n',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 20,
+                  color: const Color(0xfff37a83),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(-40.0, 406.0),
+            child: Container(
+              width: 136.5,
+              height: 118.5,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(55.0),
+                image: DecorationImage(
+                  image: const AssetImage(''),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(116.0, 406.0),
+            child: Container(
+              width: 178.5,
+              height: 118.5,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(239.0),
+                image: DecorationImage(
+                  image: const AssetImage(''),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(315.0, 406.0),
+            child: Container(
+              width: 178.5,
+              height: 118.5,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(239.0),
+                image: DecorationImage(
+                  image: const AssetImage(''),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(-6.0, 561.0),
+            child: SizedBox(
+              width: 148.0,
+              child: Text(
+                'Followers (3)>\n',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 20,
+                  color: const Color(0xfff37a83),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(10.0, 596.0),
+            child: Container(
+              width: 80.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40.0),
+                image: DecorationImage(
+                  image: const AssetImage(''),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(116.0, 597.0),
+            child: Container(
+              width: 80.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40.0),
+                image: DecorationImage(
+                  image: const AssetImage(''),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(114.0, 688.0),
+            child: SizedBox(
+              width: 86.0,
+              height: 39.0,
+              child: SingleChildScrollView(
+                  child: Text(
+                '@Fre3d\n',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 20,
+                  color: const Color(0xff5ca9af),
+                ),
+                textAlign: TextAlign.center,
+              )),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(6.0, 689.0),
+            child: SizedBox(
+              width: 86.0,
+              height: 27.0,
+              child: SingleChildScrollView(
+                  child: Text(
+                '@m9ll9\n',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 20,
+                  color: const Color(0xff5ca9af),
+                ),
+                textAlign: TextAlign.center,
+              )),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(222.0, 597.0),
+            child: Container(
+              width: 80.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40.0),
+                image: DecorationImage(
+                  image: const AssetImage(''),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(219.0, 687.0),
+            child: SizedBox(
+              width: 86.0,
+              height: 27.0,
+              child: SingleChildScrollView(
+                  child: Text(
+                '@laty6\n\n',
+                style: TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 20,
+                  color: const Color(0xff5ca9af),
+                ),
+                textAlign: TextAlign.center,
+              )),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const String _svg_xnw4oi =
+    '<svg viewBox="-259.0 372.2 327.7 202.4" ><path transform="translate(-258.98, 372.19)" d="M 108 0 C 167.6467437744141 0 216 47.64676284790039 216 106.4220123291016 C 216 165.197265625 138.6454620361328 106.4220123291016 78.99871826171875 106.4220123291016 C 19.35197067260742 106.4220123291016 0 165.197265625 0 106.4220123291016 C 0 47.64676284790039 48.35325241088867 0 108 0 Z" fill="#f37a83" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /><path transform="translate(-82.26, 430.56)" d="M 75.5 100.1680526733398 C 117.1974945068359 100.1680526733398 151 32.23550033569336 151 72 C 151 111.7644958496094 117.1974945068359 144 75.5 144 C 33.8025016784668 144 0 111.7644958496094 0 72 C 0 32.23550033569336 33.80250549316406 100.1680526733398 75.5 100.1680526733398 Z" fill="#5ca9af" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
